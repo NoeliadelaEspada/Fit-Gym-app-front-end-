e199773a8a37488d18e022f0e8d4e2ee0830c068
NoeliadelaEspada <noe_delaespada@hotmail.com>
2025-04-15 13:20:29 +0200

    Update cliente_dao.py

--- cliente_dao.py
+++ cliente_dao.py
@@ -1,10 +1,10 @@
-from conexion import Conexion   #Importamos la clase Conexion
-from cliente import Cliente #Importamos la clase Cliente
+from conexion import Conexion   
+from cliente import Cliente 
 
 
 
-class ClienteDAO: # Esta clase podrá interactuar con el objeto cliente 
-    SELECCIONAR = 'SELECT * FROM cliente ORDER BY id'   #con ORDER BY id ordenamos los valores de la tabla por el id
+class ClienteDAO: 
+    SELECCIONAR = 'SELECT * FROM cliente ORDER BY id'   
     SELECCIONAR_ID = 'SELECT * FROM cliente WHERE id=%s'
     INSERTAR = 'INSERT INTO cliente(nombre, apellido, membresía) VALUES(%s, %s, %s)'
     ACTUALIZAR = 'UPDATE cliente SET nombre=%s, apellido=%s, membresía=%s WHERE id=%s'
@@ -34,37 +34,34 @@ class ClienteDAO: # Esta clase podrá interactuar con el objeto cliente
     def seleccionar(cls):
         conexion = None
         try:
-            conexion = Conexion.obtener_conexion() # Creamos un objeto conexión y con la ayuda de la clase Conexión, usamos el método obtener conexión
-                                                    # Una vez creado esto, sabemos que el bloque finally si se ejecuta ya que si que existe una conexión.
-            cursor = conexion.cursor() #creamos un objeto cursor y con la ayuda de la conexión creamos un cursor.
-            cursor.execute(cls.SELECCIONAR)  #con ayuda del cursor que utiliza el método execute, ejecutamos la constante de clase seleccionar.
-            registros = cursor.fetchall() #hacemos que todos los registros se procesen y almacenen en esta variable
-                                         # no solo haremos eso, sino que por cada registro que recuperemos crearemos un objeto de tipo cliente (que proviene de la clase Cliente)
-            # MAPEO de clase-tabla cliente
+            conexion = Conexion.obtener_conexion()
+            cursor = conexion.cursor() 
+            cursor.execute(cls.SELECCIONAR)  
+            registros = cursor.fetchall() 
+                                        
             clientes = []
-            for registro in registros: # Con un bucle for creamos un objeto cliente por cada fila y en él almacenaremos los registros 0, 1, 2, 3 [id, nombre, apellido y membresía]
+            for registro in registros: 
                 cliente = Cliente(registro[0], registro[1], registro[2], registro[3])
-                clientes.append(cliente) # Agregamos a nuestra lista clientes todos los objetos clientes
+                clientes.append(cliente) 
             return clientes
         except Exception as e:
             print(f'Ha sucedido un error al seleccionar los datos: {e}')
 
-        finally: # este finally revisará si el objeto conexión es distinto a None. Si lo es, eso implica que ya se utilizó y por lo tanto hay que liberarlo.
+        finally: 
             if conexion is not None:
                 cursor.close()
-                Conexion.liberar_conexion(conexion) #Con ayuda de la clase Conexión, llamamos al método cerrar conexión y la cerramos
+                Conexion.liberar_conexion(conexion) 
         
-
     @classmethod
     def insertar(cls, cliente): #Este método recibe un objeto de tipo cliente
         conexion = None
         try:
             conexion = Conexion.obtener_conexion()
             cursor = conexion.cursor()
-            valores = (cliente.nombre, cliente.apellido, cliente.membresía) #añadimos los valores
-            cursor.execute(cls.INSERTAR, valores) #llamamos a la constantede clase ELIMINAR y ofrecemos los valores como parámetro
-            conexion.commit() #Guardamos los datos en la base de datos
-            return cursor.rowcount # Esto nos devuelve los valores insertados
+            valores = (cliente.nombre, cliente.apellido, cliente.membresía) 
+            cursor.execute(cls.INSERTAR, valores) 
+            conexion.commit() 
+            return cursor.rowcount 
         
         except Exception as e:
             print(f'Ha ocurrido un error insertando los datos: {e}')
@@ -135,8 +132,8 @@ if __name__ == '__main__':
     print(f'Se ha eliminado el siguiente cliente: {cliente_eliminado}')
 """
 
-    # Mostrar información de la base de datos:
-    clientes = ClienteDAO.seleccionar()     #creamos un objeto de tipo cliente y seleccionamos a los clientes
-    for cliente in clientes: #Por cada uno de los objetos de tipo cliente que tenemos en nuestra lista, lo iteramos e imprimimos.
+    
+    clientes = ClienteDAO.seleccionar()     
+    for cliente in clientes:
         print(cliente)
 
